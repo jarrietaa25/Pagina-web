59ea7ce2839bdaf0c6e1ccf16b8622aecf88f938
juana <jarrietaa25@gmail.com>
2025-11-19 12:12:40 -0300

    19.11.2

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ include("conexion.php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="Programacion/css/estilos.css">
     <title>CONSTRUCTIVISMO</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
     <link rel="icon" href="./imagenes/icono.png">
